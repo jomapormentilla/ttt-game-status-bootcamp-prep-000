1bdf73ea26ccd908b67c36be7c0894d585de87ad
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-30 17:41:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,13 +51,15 @@ def over?(board)
 end
 
 def winner(board)
-  won?(board).each do |index|
-    if(board[index[0]] == "X" && board[index[1]] == "X" && board[index[2]] == "X")
-      return "X"
-    elsif(board[index[0]] == "O" && board[index[1]] == "O" && board[index[2]] == "O")
-      return "O"
-    else
-      nil
+  if(draw?(board) == true)
+    nil
+  else
+    won?(board).each do |index|
+      if(board[index[0]] == "X" && board[index[1]] == "X" && board[index[2]] == "X")
+        return "X"
+      elsif(board[index[0]] == "O" && board[index[1]] == "O" && board[index[2]] == "O")
+        return "O"
+      end
     end
   end
 end
